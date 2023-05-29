eb646a64f53caccf1b0c30a8e7f97471bb223b6c
Andrew Burdo <zeezooz@gmail.com>
2023-05-29 22:02:16 +0300

    Add data

---
+++ lib/src/data/const.dart
@@ -0,0 +1,38 @@
+const int dateMaxYear = 2050;
+const int dateMinYear = 1000;
+const int bruteforceCardinality = 10;
+const int minGuessesBeforeGrowingSequence = 10000;
+const int minSubmatchGuessesSingleChar = 10;
+const int minSubmatchGuessesMultiChar = 50;
+const int minYearSpace = 20;
+// \xbf-\xdf is a range for almost all special uppercase letter like Ä and so on
+final RegExp startUpper = RegExp(r'^[A-Z\xbf-\xdf][^A-Z\xbf-\xdf]+$');
+final RegExp endUpper = RegExp(r'^[^A-Z\xbf-\xdf]+[A-Z\xbf-\xdf]$');
+// \xdf-\xff is a range for almost all special lowercase letter like ä and so on
+final RegExp allUpper = RegExp(r'^[A-Z\xbf-\xdf]+$');
+final RegExp allUpperInverted = RegExp(r'^[^a-z\xdf-\xff]+$');
+final RegExp allLower = RegExp(r'^[a-z\xdf-\xff]+$');
+final RegExp allLowerInverted = RegExp(r'^[^A-Z\xbf-\xdf]+$');
+final RegExp oneLower = RegExp(r'[a-z\xdf-\xff]');
+final RegExp oneUpper = RegExp(r'[A-Z\xbf-\xdf]');
+final RegExp alphaInverted =
+    RegExp(r'[^A-Za-z\xbf-\xdf]', caseSensitive: false);
+final RegExp allDigit = RegExp(r'^\d+$');
+final int referenceYear = DateTime.now().year;
+final Map<String, RegExp> regexen = <String, RegExp>{
+  'recentYear': RegExp(r'19\d\d|200\d|201\d|202\d'),
+};
+/* Separators */
+const List<String> separatorChars = <String>[
+  ' ',
+  ',',
+  ';',
+  ':',
+  '|',
+  '/',
+  r'\',
+  '_',
+  '.',
+  '-',
+];
+final int separatorCharCount = separatorChars.length;
---
+++ lib/src/data/date_splits.dart
@@ -0,0 +1,29 @@
+const Map<int, List<List<int>>> dateSplits = <int, List<List<int>>>{
+  4: <List<int>>[
+    // for length-4 strings, eg 1191 or 9111, two ways to split:
+    <int>[1, 2], // 1 1 91 (2nd split starts at index 1, 3rd at index 2)
+    <int>[2, 3], // 91 1 1
+  ],
+  5: <List<int>>[
+    <int>[1, 3], // 1 11 91
+    <int>[2, 3], // 11 1 91
+    // <int>[2, 3], // 91 1 11    <- duplicate previous one
+    <int>[2, 4], // 91 11 1    <- New and must be added as bug fix
+  ],
+  6: <List<int>>[
+    <int>[1, 2], // 1 1 1991
+    <int>[2, 4], // 11 11 91
+    <int>[4, 5], // 1991 1 1
+  ],
+  //  1111991
+  7: <List<int>>[
+    <int>[1, 3], // 1 11 1991
+    <int>[2, 3], // 11 1 1991
+    <int>[4, 5], // 1991 1 11
+    <int>[4, 6], // 1991 11 1
+  ],
+  8: <List<int>>[
+    <int>[2, 4], // 11 11 1991
+    <int>[4, 6], // 1991 11 11
+  ],
+};
